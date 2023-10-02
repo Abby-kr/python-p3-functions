a72ff7b52e9f3764fcffe7559ec38d75966f824e
Abby-kr <abby.a.otieno@gmail.com>
2023-10-02 16:35:03 +0300

    Completed Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!", end="\n")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!", end="\n")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!", end="\n")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
